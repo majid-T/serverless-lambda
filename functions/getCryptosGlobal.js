84ca7d66ef7e20d61e8e4f6f3f514e5ca48888b6
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-10-05 01:52:19 -0400

    Adding axios to call the 3rd party API

--- functions/getCryptosGlobal.js
+++ functions/getCryptosGlobal.js
@@ -1,5 +1,15 @@
 //Coin Gecko API call for endpoint https://api.coingecko.com/api/v3/global
-module.exports.handler = function (event, context, callback) {
-    const responseObjext = { statusCode: 200, body: "Hello world!" };
-    callback(null, responseObjext);
+const axios = require('axios');
+
+module.exports.handler = async function (event, context, callback) {
+    //Making sure just GET methods are processed by our function
+    if (event.httpMethod === "GET") {
+        const response = await axios.get("https://api.coingecko.com/api/v3/global");
+        const responseObjext = { statusCode: 200, body: JSON.stringify(response.data) };
+        callback(null, responseObjext);
+    } else {
+        const reqMethod = event.httpMethod;
+        callback(null, { statusCode: 405, body: JSON.stringify({ errMsg: `The ${reqMethod} http method is not supported on this function` }) })
+    }
+
 }
